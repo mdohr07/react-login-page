3db0ab156b21b3bb5d52ec3cdf63352ed00f20c3
mdohr07 <124890015+mdohr07@users.noreply.github.com>
2025-07-11 11:58:53 +0200

    Add and asjust styles and add a basic function for buttons

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -9,47 +9,51 @@ const LoginSignup = () => {
   return (
     <div className="container">
       <header>Header</header>
+
       <form>
         <h2>Sign up</h2>
 
-        <div className="input">
-          <label htmlFor="username">
-            <img src={user_icon} alt="User icon" />
-            <input
-              type="text"
-              id="username"
-              name="username"
-              placeholder="Username"
-            />
-          </label>
+        <div className="inputs">
+          <div className="input">
+            <label htmlFor="username">
+              <img src={user_icon} alt="User icon" />
+              <input
+                type="text"
+                id="username"
+                name="username"
+                placeholder="Username"
+              />
+            </label>
+          </div>
+
+          <div className="input">
+            <label htmlFor="email">
+              <img src={email_icon} alt="Email icon" />
+              <input type="email" id="email" name="email" placeholder="Email" />
+            </label>
+          </div>
+
+          <div className="input">
+            <label htmlFor="password">
+              <img src={password_icon} alt="Password icon" />
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+              />
+            </label>
+          </div>
         </div>
 
-        <div className="input">
-          <label htmlFor="email">
-            <img src={email_icon} alt="Email icon" />
-            <input type="email" id="email" name="email" placeholder="Email" />
-          </label>
-        </div>
-
-        <div className="input">
-          <label htmlFor="password">
-            <img src={password_icon} alt="Password icon" />
-            <input
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Password"
-            />
-          </label>
-        </div>
-
-        <section className="forgot-password">Forgot your Password?</section>
         <button type="submit" className="btn-signUp">
           Sign up
         </button>
         <button type="submit" className="btn-LogIn">
           Log in
         </button>
+
+        <section className="forgot-password">Forgot your Password?</section>
       </form>
     </div>
   );
